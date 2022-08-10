e55ef81cc8da378e89d13e88835e3662db964889
banky <bankiadebajo1@gmail.com>
2022-08-09 21:31:26 -0400

    Add components folder

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+export const Hero = () => {
+  return (
+    <>
+      <div className="flex">
+        <img src="/images/logo.png" />
+      </div>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,13 @@ import type { NextPage } from 'next';
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-export const YourApp = () => {
-  return;
-};
-
 const Home: NextPage = () => {
   return (
     <>
       <div className="flex justify-between">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="w-10">
+          <img src="/images/logo.png" />
+        </div>
         <ConnectButton />
       </div>
     </>
